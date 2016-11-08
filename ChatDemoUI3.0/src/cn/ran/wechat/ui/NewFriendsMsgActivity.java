9cf03d6b44aad75842b42ab4f2b114f6a15f4efc
ranyiran <271536025@qq.com>
2016-11-08 14:56:14 +0800

    新朋友的Adpater的修改

--- ChatDemoUI3.0/src/cn/ran/wechat/ui/NewFriendsMsgActivity.java
+++ ChatDemoUI3.0/src/cn/ran/wechat/ui/NewFriendsMsgActivity.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,37 +15,56 @@ package cn.ran.wechat.ui;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import cn.ran.wechat.R;
 import cn.ran.wechat.adapter.NewFriendsMsgAdapter;
 import cn.ran.wechat.db.InviteMessgeDao;
 import cn.ran.wechat.domain.InviteMessage;
-
-import java.util.List;
+import cn.ran.wechat.utils.MFGT;
 
 /**
  * Application and notification
- *
  */
 public class NewFriendsMsgActivity extends BaseActivity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.em_activity_new_friends_msg);
+    @InjectView(R.id.ivBack)
+    ImageView ivBack;
+    @InjectView(R.id.tvCenter)
+    TextView tvCenter;
+    @InjectView(R.id.list)
+    ListView listView;
 
-		ListView listView = (ListView) findViewById(R.id.list);
-		InviteMessgeDao dao = new InviteMessgeDao(this);
-		List<InviteMessage> msgs = dao.getMessagesList();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.em_activity_new_friends_msg);
+        ButterKnife.inject(this);
+        ivBack.setVisibility(View.VISIBLE);
+        tvCenter.setVisibility(View.VISIBLE);
+        tvCenter.setText(getString(R.string.recommended_friends));
 
-		NewFriendsMsgAdapter adapter = new NewFriendsMsgAdapter(this, 1, msgs); 
-		listView.setAdapter(adapter);
-		dao.saveUnreadMessageCount(0);
-		
-	}
+        InviteMessgeDao dao = new InviteMessgeDao(this);
+        List<InviteMessage> msgs = dao.getMessagesList();
+        NewFriendsMsgAdapter adapter = new NewFriendsMsgAdapter(this, 1, msgs);
+        listView.setAdapter(adapter);
+        dao.saveUnreadMessageCount(0);
 
-	public void back(View view) {
-		finish();
-	}
+    }
+
+    public void back(View view) {
+        finish();
+    }
+
+    @OnClick(R.id.ivBack)
+    public void onClick() {
+        MFGT.finish(this);
+    }
 }
