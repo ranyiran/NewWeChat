951326b909413ca9c0a7770798d85ec6228bd2dc
ranyiran <271536025@qq.com>
2016-11-09 19:24:58 +0800

    将EaseUi中的EaseUser为User

--- ChatDemoUI3.0/src/cn/ran/wechat/ui/ForwardMessageActivity.java
+++ ChatDemoUI3.0/src/cn/ran/wechat/ui/ForwardMessageActivity.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,25 +17,26 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import cn.ran.wechat.R;
-import com.hyphenate.easeui.domain.EaseUser;
+
+import com.hyphenate.easeui.domain.User;
 import com.hyphenate.easeui.widget.EaseAlertDialog;
 import com.hyphenate.easeui.widget.EaseAlertDialog.AlertDialogUser;
 
 public class ForwardMessageActivity extends PickContactNoCheckboxActivity {
-	private EaseUser selectUser;
-	private String forward_msg_id;
+    private User selectUser;
+    private String forward_msg_id;
 
-	 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		forward_msg_id = getIntent().getStringExtra("forward_msg_id");
-	}
-	
-	@Override
-	protected void onListItemClick(int position) {
-		selectUser = contactAdapter.getItem(position);
-		new EaseAlertDialog(this, null, getString(R.string.confirm_forward_to, selectUser.getNick()), null, new AlertDialogUser() {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        forward_msg_id = getIntent().getStringExtra("forward_msg_id");
+    }
+
+    @Override
+    protected void onListItemClick(int position) {
+        selectUser = contactAdapter.getItem(position);
+        new EaseAlertDialog(this, null, getString(R.string.confirm_forward_to, selectUser.getMUserNick()), null, new AlertDialogUser() {
             @Override
             public void onResult(boolean confirmed, Bundle bundle) {
                 if (confirmed) {
@@ -47,13 +48,13 @@ public class ForwardMessageActivity extends PickContactNoCheckboxActivity {
                     }
                     Intent intent = new Intent(ForwardMessageActivity.this, ChatActivity.class);
                     // it is single chat
-                    intent.putExtra("userId", selectUser.getUsername());
+                    intent.putExtra("userId", selectUser.getMUserName());
                     intent.putExtra("forward_msg_id", forward_msg_id);
                     startActivity(intent);
                     finish();
                 }
             }
         }, true).show();
-	}
+    }
 
 }
